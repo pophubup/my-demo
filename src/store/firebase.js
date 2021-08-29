b9394eed9fd297ab048a15c674ef4f5db5d4f289
pophubup <Yohoo0987@gmail.com>
2021-08-29 11:11:48 +0800

    temp change config display

--- src/store/firebase.js
+++ src/store/firebase.js
@@ -2,7 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs, writeBatch, doc } from 'firebase/firestore';
 import { getStorage, ref, uploadBytes } from "firebase/storage";
 console.log(process.env.API_AUTH, process.env)
-const firebaseConfig = process.env.NODE_ENV === "production" ? process.env.API_AUTH : require('../../connection.json');
+    //const firebaseConfig = process.env.NODE_ENV === "production" ? process.env.API_AUTH : require('../../connection.json');
+const firebaseConfig = {
+    "apiKey": "AIzaSyCjjT5MiOsBNAarIO--4dWSvYPRrQr2sFo",
+    "authDomain": "getproducts-92bee.firebaseapp.com",
+    "projectId": "getproducts-92bee",
+    "storageBucket": "getproducts-92bee.appspot.com"
+};
 const base = initializeApp(firebaseConfig);
 const firestore = getFirestore(base);
 const storage = getStorage(base);
