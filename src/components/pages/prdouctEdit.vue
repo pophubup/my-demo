b9c8357899853c944f91a2b95f56acc0d9d3832f
Goodarc\Yukai <yuki@goodarc.com>
2021-09-01 17:26:04 +0800

    停用 fireStore

--- src/components/pages/prdouctEdit.vue
+++ src/components/pages/prdouctEdit.vue
@@ -1,81 +1,90 @@
 <template>
-
   <div class="p-col-12" style="padding-top: 90px">
     <div style="padding-right: 30px; padding-left: 30px">
-      <div class="p-shadow-13" >
-      <div class="card" style="padding:30px">
-              <h2>商品修改頁</h2>
-            <form class="p-fluid p-formgrid p-grid" @submit.prevent="onSubmit">
-              
-                <div class="p-field p-col-12 p-md-3">
-                    <label for="名稱">名稱</label>
-                    <InputText id="名稱" type="text" v-model.trim="name"/>
-                </div>
-                
-                <div class="p-field p-col-12">
-                    <label for="內容">內容</label>
-                    <Textarea id="內容" rows="5" :autoResize="true" v-model.trim="description"/>
-                </div>
-                <div class="p-field p-col-12 p-md-6">
-                    <label for="city">圖片</label>
-                    <FileUpload mode="basic" name="demo[]" accept="image/*" :auto="true"  :maxFileSize="1000000" :customUpload="true"  @uploader="onUpload" />
-                </div>
-                <div class="p-col-1 p-offset-11">
-                     <Button type="submit" label="確定"  />
-                </div>
-            </form>
-            <OrderList v-model="cars" listStyle="height:auto" dataKey="vin">
-    <template #header>
-        List of Cars
-    </template>
-    <template #item="slotProps">
-        <div class="p-caritem">
-            <img :src="slotProps.item.file.objectURL" style="width">
-            <div>
-                <span class="p-caritem-vin">{{slotProps.item.name}}</span>
-                <span> {{slotProps.item.description}}</span>
+      <div class="p-shadow-13">
+        <div class="card" style="padding: 30px">
+          <h2>商品修改頁</h2>
+          <form class="p-fluid p-formgrid p-grid" @submit.prevent="onSubmit">
+            <div class="p-field p-col-12 p-md-3">
+              <label for="名稱">名稱</label>
+              <InputText id="名稱" type="text" v-model.trim="name" />
             </div>
-        </div>
-    </template>
-</OrderList>
 
+            <div class="p-field p-col-12">
+              <label for="內容">內容</label>
+              <Textarea
+                id="內容"
+                rows="5"
+                :autoResize="true"
+                v-model.trim="description"
+              />
+            </div>
+            <div class="p-field p-col-12 p-md-6">
+              <label for="city">圖片</label>
+              <FileUpload
+                mode="basic"
+                name="demo[]"
+                accept="image/*"
+                :auto="true"
+                :maxFileSize="1000000"
+                :customUpload="true"
+                @uploader="onUpload"
+              />
+            </div>
+            <div class="p-col-1 p-offset-11">
+              <Button type="submit" label="確定" />
+            </div>
+          </form>
+          <OrderList v-model="cars" listStyle="height:auto" dataKey="vin">
+            <template #header> List of Cars </template>
+            <template #item="slotProps">
+              <div class="p-caritem">
+                <img :src="slotProps.item.file.objectURL" style="width" />
+                <div>
+                  <span class="p-caritem-vin">{{ slotProps.item.name }}</span>
+                  <span> {{ slotProps.item.description }}</span>
+                </div>
+              </div>
+            </template>
+          </OrderList>
         </div>
-     </div>
-     </div>
-     </div>
- 
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-   data() {
-     return {
-       cars:[ ],
-       name :'',
-       description:'',
-       file:'',
-     }
-   },
-   methods: {
-     onSubmit(){
-          const obj = {
-             name : this.name,
-             description : this.description,
-            file : this.file
-          }
-          this.cars.push(obj)
-          console.log(obj)
-     },
-     onUpload(event) {
-     
-         this.file = event.files[0]
-         this.$toast.add({severity: 'info', summary: 'Success', detail: 'File Uploaded', life: 3000});
-      },
-   },
-
+  data() {
+    return {
+      cars: [],
+      name: "",
+      description: "",
+      file: "",
+    };
+  },
+  methods: {
+    onSubmit() {
+      const obj = {
+        name: this.name,
+        description: this.description,
+        file: this.file,
+      };
+      this.cars.push(obj);
+      console.log(obj);
+    },
+    onUpload(event) {
+      this.file = event.files[0];
+      this.$toast.add({
+        severity: "info",
+        summary: "Success",
+        detail: "File Uploaded",
+        life: 3000,
+      });
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
--- src/router.js
+++ src/router.js
@@ -6,20 +6,16 @@ import productEdit from './components/pages/prdouctEdit.vue';
 import NotFound from './components/NotFound.vue'
 import contact from './components/pages/contact.vue';
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes: [
-        { path: "/", redirect: "/main" },
-        { path: "/main", component: main },
-        { path: "/product", component: product },
-<<<<<<< HEAD
-        { path: "/productInsert", component: productInsert },
-        { path: "/productEdit", component: productEdit },
-=======
-        { path: "/productInsertOrEdit", component: productInsertOrEdit },
-        {path:"/contact",component:contact},
->>>>>>> cy/feature/Contact
-        { path: "/:notFound(.*)", component: NotFound },
-    ],
+        history: createWebHistory(process.env.BASE_URL),
+        routes: [
+                { path: "/", redirect: "/main" },
+                { path: "/main", component: main },
+                { path: "/product", component: product },
+                { path: "/productInsert", component: productInsert },
+                { path: "/productEdit", component: productEdit },
+                { path: "/contact", component: contact },
+                { path: "/:notFound(.*)", component: NotFound },
+        ],
 });
 
 export default router;
