74fe6a00994b49b1c4cb1f60db050e570b7c0b52
pophubup <Yohoo0987@gmail.com>
2021-10-07 21:26:09 +0800

    merge product page

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
-
-process.env.VUE_APP_URL = process.env.NODE_ENV === "production" ? "https://yohoho123.azurewebsites.net" : "https://localhost:5001";
-
-    
+process.env.VUE_APP_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://yohoho123.azurewebsites.net"
+    : "https://localhost:5001";
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/my-demo/" : "/",
-    lintOnSave: false
-
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/my-demo/" : "/",
+  lintOnSave: false,
+};
